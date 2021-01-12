ecf0a9966716bd6a19d6c90979b98030a7553b70
forgottennick <77056126+forgottennick@users.noreply.github.com>
2021-01-12 13:46:10 +0100

    python 2to3

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -1,6 +1,6 @@
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import gzip
-import StringIO
+import io
 
 #
 # HTTPCommunicator
@@ -10,23 +10,23 @@ class HTTPCommunicator :
     # GET
     #
     def get( self, url ):
-        httpHandler = urllib2.HTTPHandler(debuglevel=0)
+        httpHandler = urllib.request.HTTPHandler(debuglevel=0)
         
         try :
-            request = urllib2.Request( url )
+            request = urllib.request.Request( url )
             request.add_header( "Accept"         , "*/*" )
             request.add_header( "Accept-Encoding", "gzip" )
             request.add_header( "User-Agent"     , "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0")
-            opener = urllib2.build_opener(httpHandler)
+            opener = urllib.request.build_opener(httpHandler)
             f = opener.open(request)
         # Exception
-        except urllib2.HTTPError, e :
+        except urllib.error.HTTPError, e :
             raise Exception( "HTTP Error %u: %s" % ( e.code, HTTP_REPONSES[ e.code ] ) )
 
         # Compressed (gzip) response...
         if f.headers.get( "content-encoding" ) == "gzip" :
             httpGzippedData = f.read()
-            stringIO        = StringIO.StringIO( httpGzippedData )
+            stringIO        = io.StringIO( httpGzippedData )
             gzipper         = gzip.GzipFile( fileobj = stringIO )
             httpData        = gzipper.read()
             
